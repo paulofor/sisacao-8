978d060c8922cf97b03ed3a49211b09080b75669
paulofor <paulofore@gmail.com>
2025-08-19 11:10:38 -0700

    CI: run gcloud deploy commands non-interactively

---
+++ tests/test_google_scraper.py
@@ -0,0 +1,13 @@
+import pytest
+
+from functions.google_finance_price import google_scraper as gf_scraper
+
+
+def test_extract_price_from_html_success():
+    html = '<div class="YMlKec fxKbKc">R$ 10,50</div>'
+    assert gf_scraper.extract_price_from_html(html) == pytest.approx(10.50)
+
+
+def test_extract_price_from_html_missing():
+    with pytest.raises(ValueError):
+        gf_scraper.extract_price_from_html("<div></div>")
